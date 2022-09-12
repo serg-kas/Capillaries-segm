2278c9496ecb13d296c6c0eebfd67a2b03baed04
serg-kas <serg_kas@mail.ru>
2022-09-12 22:28:15 +0300

    dice_coef_loss-2

--- zoo.py
+++ zoo.py
@@ -147,7 +147,7 @@ def unet(num_classes=2, input_shape=(1024, 1024, 3)):
     # Компилируем модель
     model.compile(optimizer=Adam(),
                   # loss='categorical_crossentropy',
-                  loss=[dice_coef_loss],
+                  loss=dice_coef_loss,
                   metrics=[dice_coef])
     return model
 
